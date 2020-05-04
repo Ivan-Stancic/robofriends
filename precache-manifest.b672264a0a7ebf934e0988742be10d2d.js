740bacbaddc4c882840bc371aad3c5e9391cd87b
Sovazg <62153026+Sovazg@users.noreply.github.com>
2020-05-04 14:50:58 +0200

    Updates

---
+++ precache-manifest.b672264a0a7ebf934e0988742be10d2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63ab0a0a72720b42b55cee23a26dfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdfa82f2b04c1e900661",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "073cfac9be73ad3f5bcf",
+    "url": "/static/css/main.2440a3f9.chunk.css"
+  },
+  {
+    "revision": "fdfa82f2b04c1e900661",
+    "url": "/static/js/2.79e767d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79e767d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073cfac9be73ad3f5bcf",
+    "url": "/static/js/main.e7150e80.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.woff"
+  }
+]);
